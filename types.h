955bc6607cbdec26dcf39ba1b8d4c875a831425c
Astg582 <109224508+Astg582@users.noreply.github.com>
2024-01-29 19:56:40 +0400

    Set get types.h

--- types.h
+++ types.h
@@ -4,12 +4,31 @@
 class token {
 	private:
 		int pos;
-		type_s ;
+		type_s ty;
 		std::string tok_n; 
-       public:
-                void set_pos(int p){
+        public:
+                void set_pos(const int p){
 			this->pos = p;
 		}
-                void set_type (t
+
+                void set_type (const type_s t){
+			this->ty = t;
+		}
+
+                void set_tokn(const std::string& oc){
+			this->tok_n = oc;
+		}
+
+                int get_pos(){
+                        return this->pos;
+		}
+
+                type_s get_type(){
+			return this->ty;
+		}
+
+                std::string get_tokn (){
+			return this->tok_n;
+		}
 };
 
